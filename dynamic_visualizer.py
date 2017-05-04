bec44222e2e7cfd814ffef5ecfee6d5a5ac32b08
vesaf <vesaf@msn.com>
2017-05-04 13:55:52 +0200

    Doing some serious refactoring - Victor (2017)

--- dynamic_visualizer.py
+++ dynamic_visualizer.py
@@ -2,101 +2,16 @@ import sys
 import numpy as np
 from tkinter import *
 
-import includes.dynamic_solver as dyn
+import dynamic_solver as dyn
 import includes.visualizer as vis
-
-# Protein object contains a list of aminoacids, that each have a type and location
-class Protein (object):
-    protein = []
-    n = 0
-    p = ""
-
-    # Initialize
-    def __init__(self):
-        self.p = input("Protein: ")
-        self.n = len(self.p)
-
-        # Protein has to be at least 3 long to be relevant
-        if self.n < 3:
-            quit()
-
-        # Protein has to be composed out of H and P and C only
-        for i in self.p:
-            if i != "H" and i != "P" and i != "C":
-                quit()
-            else:
-                self.protein.append(self.Amino(i))
-
-    def translate(self, folds):
-        direction = 1
-        last_w = n // 2
-        last_h = n // 2
-        skip_next = False
-
-        for i in range(n):
-            if skip_next == False:
-                self.protein[i].assignPlace(last_w, last_h + direction)
-                last_h += direction
-                if i in folds:
-                    self.protein[i + 1].assignPlace(last_w + 1, last_h)
-                    skip_next = True
-                    last_w += 1
-                    direction *= -1
-            else:
-                skip_next = False
-
-        print("Score: -{}".format(vis.scoreFn(list(self.protein), 0)))
-
-        drawn = 0
-        for amino in self.protein:
-            amino.drawAmino(self.protein, drawn)
-            drawn += 1
-        vis.scoreFn(list(self.protein), 0)
-
-    class Amino(object):
-        prevAmino = None
-
-        # Initialize
-        def __init__(self, type):
-            self.type = type
-
-        def draw_line(self, canvas, a, b, c, d):
-            canvas.create_line(vis.r(self.loc_w * grid_size + grid_size / a),
-                               vis.r(self.loc_h * grid_size + grid_size / b),
-                               vis.r(self.prevAmino.loc_w * grid_size + grid_size / c),
-                               vis.r(self.prevAmino.loc_h * grid_size + grid_size / d), fill="red")
-
-        # Draw an aminoacid
-        def drawAmino(self, protein, drawn):
-            global canvas
-            self.prevAmino = protein[drawn - 1]
-
-            # Draw letter
-            canvas.create_text(vis.r(self.loc_w * grid_size + grid_size / 2), vis.r(self.loc_h * grid_size + grid_size / 2), text = self.type, font = vis.r(grid_size), fill="red")
-
-
-            # Connect with line to previous aminoacid
-            if drawn > 0:
-                if self.loc_h < self.prevAmino.loc_h:
-                    self.draw_line(canvas, 2, 1.5, 2, 3)
-                elif self.loc_h > self.prevAmino.loc_h:
-                    self.draw_line(canvas, 2, 3, 2, 1.5)
-                elif self.loc_w < self.prevAmino.loc_w:
-                    self.draw_line(canvas, 1.5, 2, 3, 2)
-                else:
-                    self.draw_line(canvas, 3, 2, 1.5, 2)
-
-        # Set location of aminoacid and request to draw
-        def assignPlace(self, loc_w, loc_h):
-            self.loc_w = loc_w
-            self.loc_h = loc_h
+import includes.protein as protein
 
 # Setup grid
 def createGrid():
     for i in range(n + 1):
         for j in range(n + 1):
-            canvas.create_rectangle(vis.r(j * grid_size), vis.r(i * grid_size),
-                    vis.r((j + 1) * grid_size), vis.r((i + 1) * grid_size), fill='white')
+            canvas.create_rectangle(vis.r(j * 25), vis.r(i * 25),
+                    vis.r((j + 1) * 25), vis.r((i + 1) * 25), fill='white')
     canvas.pack()
 
 if __name__ == '__main__':
@@ -106,16 +21,17 @@ if __name__ == '__main__':
     root.title("Proteam Power")
 
     # Globals
-    grid_size = 25
 
-    prot = Protein()
+    canvas = Canvas(root)
+
+    prot = protein.Protein(canvas)
     n = prot.n
 
     # Initialize canvas
-    canvas = Canvas(root, width = vis.r((n + 1) * grid_size),
-                          height = vis.r((n + 1) * grid_size),
-                          scrollregion=(0, 0, vis.r((n + 1) * grid_size),
-                          vis.r((n + 1) * grid_size)))
+    canvas.config(width = vis.r((n + 1) * 25),
+                          height = vis.r((n + 1) * 25),
+                          scrollregion=(0, 0, vis.r((n + 1) * 25),
+                          vis.r((n + 1) * 25)))
 
     hbar = Scrollbar(root,orient=HORIZONTAL)
     hbar.pack(side=BOTTOM,fill=X)
@@ -132,6 +48,6 @@ if __name__ == '__main__':
 
     q, fold = dyn.solve(prot.p)
     maxval = np.argmax(q[1:n]) + 1
-    prot.translate(fold[maxval])
+    prot.translateDynamic(fold[maxval])
 
     root.mainloop()
---
+++ includes/protein.py
@@ -0,0 +1,87 @@
+import visualizer as vis
+
+# Protein object contains a list of aminoacids, that each have a type and location
+class Protein (object):
+    protein = []
+    n = 0
+    p = ""
+    canv = None
+
+    # Initialize
+    def __init__(self, canv):
+        self.p = input("Protein: ")
+        self.n = len(self.p)
+        self.canv = canv
+        # Protein has to be at least 3 long to be relevant
+        if self.n < 3:
+            quit()
+
+        # Protein has to be composed out of H and P and C only
+        for i in self.p:
+            if i != "H" and i != "P" and i != "C":
+                quit()
+            else:
+                self.protein.append(self.Amino(i))
+
+    def translateDynamic(self, folds):
+        direction = 1
+        last_w = self.n // 2
+        last_h = self.n // 2
+        skip_next = False
+
+        for i in range(self.n):
+            if skip_next == False:
+                self.protein[i].assignPlace(last_w, last_h + direction)
+                last_h += direction
+                if i in folds:
+                    self.protein[i + 1].assignPlace(last_w + 1, last_h)
+                    skip_next = True
+                    last_w += 1
+                    direction *= -1
+            else:
+                skip_next = False
+
+        print("Score: -{}".format(vis.scoreFn(list(self.protein), 0)))
+
+        drawn = 0
+        for amino in self.protein:
+            amino.drawAmino(self.protein, drawn, self.canv)
+            drawn += 1
+        vis.scoreFn(list(self.protein), 0)
+
+    class Amino(object):
+        prevAmino = None
+
+        # Initialize
+        def __init__(self, type):
+            self.type = type
+
+        def draw_line(self, canv, a, b, c, d):
+            canv.create_line(vis.r(self.loc_w * 25 + 25 / a),
+                               vis.r(self.loc_h * 25 + 25 / b),
+                               vis.r(self.prevAmino.loc_w * 25 + 25 / c),
+                               vis.r(self.prevAmino.loc_h * 25 + 25 / d), fill="red")
+
+        # Draw an aminoacid
+        def drawAmino(self, protein, drawn, canv):
+            self.prevAmino = protein[drawn - 1]
+
+            # Draw letter
+            canv.create_text(vis.r(self.loc_w * 25 + 25 / 2), vis.r(self.loc_h * 25 + 25 / 2), text = self.type, font = vis.r(25), fill="red")
+
+
+            # Connect with line to previous aminoacid
+            if drawn > 0:
+                if self.loc_h < self.prevAmino.loc_h:
+                    self.draw_line(canv, 2, 1.5, 2, 3)
+                elif self.loc_h > self.prevAmino.loc_h:
+                    self.draw_line(canv, 2, 3, 2, 1.5)
+                elif self.loc_w < self.prevAmino.loc_w:
+                    self.draw_line(canv, 1.5, 2, 3, 2)
+                else:
+                    self.draw_line(canv, 3, 2, 1.5, 2)
+
+        # Set location of aminoacid and request to draw
+        def assignPlace(self, loc_w, loc_h):
+            self.loc_w = loc_w
+            self.loc_h = loc_h
