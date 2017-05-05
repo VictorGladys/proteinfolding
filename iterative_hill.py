ff4f21f34f1a544cb9a23d4fa127cb6d2b144491
Victor Gladys <victorgladys@gmail.com>
2017-05-05 21:39:59 +0200

    haha, zoveel door de war geraakt door het refactoren ;p ik denk wel dat het er beter leesbaar op is geworden

--- iterative_hill.py
+++ iterative_hill.py
@@ -1,9 +1,10 @@
 from includes.iterative_framework import *
 import random
 
-def hill(iters, p):
+def hill(iters, p, scoref=None):
     seq, pos = init_grid(p, len(p))
-    score = init_score(p)
+    if scoref == None:
+        scoref = init_score(p)
     best_score = 0
 
     for i in range(iters):
@@ -15,21 +16,21 @@ def hill(iters, p):
             # is implemented
             for bendp in range(2, len(p)):
                 option = bend_part(bendd, bendp, seq, pos)
-                if option == None:
+                if option[0] == None:
                     continue
 
-                this_score = score(option[0])
+                this_score = scoref(option[0])
                 if this_score > best_score:
                     scores = [option]
                     best_score = this_score
                 elif this_score == best_score:
                     scores.append(option)
         seq, pos = random.choice(scores)
-        return(seq, pos)
+    return seq, pos, scoref(seq)
 
 if __name__ == '__main__':
     iters = 100
     p = input("Protein: ")
-    seq, pos = hill(iters, p)
+    seq, pos, score = hill(iters, p)
     print(seq)
-    print(score(seq))
+    print(score)
